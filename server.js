43cea8ee31d4359bb41493d2e456c8cd3556b6cc
Alexey Galizdra <galizdra_alex@mail.ru>
2019-12-11 06:49:43 +0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://lesscalie:<2019944zZ>@todoappcluster-vfvav.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("test");
-  
-});
